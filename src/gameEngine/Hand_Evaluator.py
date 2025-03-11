10695c72e6444b93393887c64c28e14f92549a4a
Conor <conordmcdevitt@gmail.com>
2025-03-11 18:55:21 -0400

    type annotation

--- src/gameEngine/Hand_Evaluator.py
+++ src/gameEngine/Hand_Evaluator.py
@@ -22,8 +22,9 @@ from Deck import Deck
     NOTE: primary_cards are the cards that make up the hand rank (in this case, the pairs)
     
 """
+
+
 class HandEvaluator():
-   
 
     STRENGTH_MAP = {
         "royal_flush": 10,
@@ -40,25 +41,24 @@ class HandEvaluator():
 
     def __init__(self):
         # these are the cards that make up the strength Ex. two pair = ["3S", "3C", "4S", "4D"]
-        # this is used for finding the kickers 
+        # this is used for finding the kickers
         self._strength_cards = []
 
         # these are the highest cards (in a 5 card hand) that aren't used to make up the strength (need for tie breaking)
         self._kicker_cards = []
 
-        #cards that are used for tie breaking similar strength hands
-        #Ex. two pair = ["3S","4S"]
+        # cards that are used for tie breaking similar strength hands
+        # Ex. two pair = ["3S","4S"]
         self._primary_cards = []
 
     @classmethod
     def hand_eval(self, hole_cards, community_cards):
 
-        
-        #sort the cards in ascending order
+        # sort the cards in ascending order
         sorted_cards = Deck.sort_cards_by_rank(hole_cards + community_cards)
         hand_rank: str = ""
 
-        #check and update strength 
+        # check and update strength
         if self._is_royal_flush(sorted_cards):
             pass
         elif self._is_straight_flush(sorted_cards):
@@ -92,53 +92,53 @@ class HandEvaluator():
             "kickers": self._kicker_cards
         }
 
-    
-    #gets the kickers based on the cards used to make up hand rank
+    # gets the kickers based on the cards used to make up hand rank
+
     @classmethod
     def _set_kickers(self, sorted_cards):
         assert (len(self._strength_cards) > 0)
 
         kickers = [
             card for card in sorted_cards if card not in self._strength_cards]
-        
+
         kickers_amount: int = 5 - len(self._strength_cards)
-        self._kicker_cards = kickers[0:kickers_amount]  
+        self._kicker_cards = kickers[0:kickers_amount]
 
     # these will update _strength_cards, and _primary_cards
     @classmethod
-    def _is_royal_flush(self, sorted_cards):
+    def _is_royal_flush(self, sorted_cards) -> bool:
         pass
 
     @classmethod
-    def _is_straight_flush(self, sorted_cards):
+    def _is_straight_flush(self, sorted_cards) -> bool:
         pass
 
     @classmethod
-    def _is_four_of_a_kind(self, sorted_cards):
+    def _is_four_of_a_kind(self, sorted_cards) -> bool:
         pass
 
     @classmethod
-    def _is_full_house(self, sorted_cards):
+    def _is_full_house(self, sorted_cards) -> bool:
         pass
 
     @classmethod
-    def _is_flush(self, sorted_cards):
+    def _is_flush(self, sorted_cards) -> bool:
         pass
 
     @classmethod
-    def _is_straight(self, sorted_cards):
+    def _is_straight(self, sorted_cards) -> bool:
         pass
 
     @classmethod
-    def _is_three_of_a_kind(self, sorted_cards):
+    def _is_three_of_a_kind(self, sorted_cards) -> bool:
         pass
 
     @classmethod
-    def _is_two_pair(self, sorted_cards):
+    def _is_two_pair(self, sorted_cards) -> bool:
         pass
 
     @classmethod
-    def _is_pair(self, sorted_cards):
+    def _is_pair(self, sorted_cards) -> bool:
         pass
 
     @classmethod
