9fcee75c90d9850ffaaf10c4a29f4ce644a91ea0
Conor <conordmcdevitt@gmail.com>
2025-03-25 16:16:28 -0400

    test_start_game/ preflop

--- src/game_engine/tests/test_engine.py
+++ src/game_engine/tests/test_engine.py
@@ -4,28 +4,31 @@ from game_engine.constants import Street
 
 class TestEngine: 
     def test_start_game(self):
+        """
+        start game is expected to initialize players
+        and start the game with the preflop street
+        """
         engine = Engine(num_players=2,initial_stack=1000, blind=1)
         engine.start_game()
 
-        #
-        expected_state = {
-            "street": Street.PREFLOP,
-            "community_cards": [],
-            "players": [
-                {
-                    "name": "pc",
-                    "stack": 1000,
-                    "hole_cards": [],
-                    "state": PlayerState.ACTIVE
-                },
-                {
-                    "name": "cpu1",
-                    "stack": 1000,
-                    "hole_cards": [],
-                    "state": PlayerState.ACTIVE
-                }
-            ]
-         }
         
-        assert engine.current_state_of_game() == expected_state
+        #testing blinds adjust players stack correctly
+        pc_stack = engine.dealer.table.players[0].stack
+        cpu1_stack = engine.dealer.table.players[1].stack
+        assert pc_stack == 999
+        assert cpu1_stack == 998
+
+        #testing if players are initialized correctly
+        pc_name = engine.dealer.table.players[0].name
+        cpu1_name = engine.dealer.table.players[1].name
+        assert pc_name == "pc"
+        assert cpu1_name == "cpu1"
+        
+        #testing if hole cards are dealt correctly
+        pc_hole_cards = engine.dealer.table.players[0].hole_cards
+        cpu1_hole_cards = engine.dealer.table.players[1].hole_cards
+
+        assert len(pc_hole_cards) == 2
+        assert len(cpu1_hole_cards) == 2
+        
        
