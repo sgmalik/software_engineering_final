9d8f36a85a9699366ad924af32300c9a37daaaa6
Conor <conordmcdevitt@gmail.com>
2025-04-02 18:39:44 -0400

    remove print

--- src/game_engine/betting_manager.py
+++ src/game_engine/betting_manager.py
@@ -44,7 +44,7 @@ class BettingManager:
             self.current_bet = self.blind*2
             self._blind(current_player, self.blind*2)
         self.table.next_player()
-        print("CURRENT BLINDER", current_player.name)
+        
 
     def _blind(self, current_player, blind):
         """
